cc1fac5f92bbbf8cdc2c056aacbdb1fd67ba885b
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-12-08 22:07:18 +0000

    Add Python data import script

---
+++ import_data.py
@@ -0,0 +1,29 @@
+import json
+import urllib
+import urllib2
+from glob import glob
+
+"""
+Import PhysioNet challenge data into CriticalCare
+
+Usage: import_data.py [HOST] [PORT]
+"""
+
+def import_files(url):
+    for filename in glob('*.txt'):
+        with open(filename) as f:
+            request = urllib2.Request(
+                    url='http://%s/file' % url,
+                    data=urllib.urlencode({'file': f.read()}))
+            response = urllib2.urlopen(request)
+            print response.read()
+            response.close()
+
+if __name__ == '__main__':
+    import sys
+    url = 'localhost:3000'
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+    if len(sys.argv) > 2:
+        url += ":%s" % sys.argv[2]
+    import_files(url)
